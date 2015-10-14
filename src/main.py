06ca6d063d863803af3a5b2f5bffc2deabc9df21
Chase Shyu <iamhemry@gmail.com>
2015-10-15 02:01:46 +0800

    deliver the charge with bond successfully.

--- src/main.py
+++ src/main.py
@@ -13,96 +13,39 @@ from ctypes import cdll
 import interface as inf
 import neural as nr
 import tools
-ip = 1
 
-brain2 = nr.Brain('proto',5)
-
-nx = 10
-ny = 5
-nz = 5
-# brain(y,x)
-brain1 = [[(j+1)+(i*nx) for j in range(nx)] for i in range(ny)]
-brain = [ [ nr.Cell((j+1)+(i*nx),j,i) for j in range(nx) ] for i in range(ny) ]
-
-#for i in range(ny):
-#    print (brain1[i][:])
-
-brain2.get_Cell(1).get_CellInfo()
-
-# Initialize the connection of cells
-for i in range(ny):
-    for j in range(nx):
-        for k in range(ny):
-            for l in range(nx):
-                if (i != k or j != l):
-                    brain[i][j].ConnectTo(brain[k][l])
-
-# Get all cells cord
-x, y = nr.get_AllCellLocation(brain2)
-# Plot the cell locations
-plt.plot(x,y,'o')
+### Paramaters ###
 
 
-#for j in range(ny):
-#    for i in range(nx):
-#        bonds = brain[j][i].get_Bond()
-#        for k in range(len(bonds)):
-#            x, y = bonds[k].get_Cord()
-#            strangth = 1. - bonds[k].get_Strength()
-#            color = [strangth, strangth, strangth]
-#            plt.plot(x,y,color=color)
-bonds = brain2.get_AllBond()
-bond_num = len(bonds)
-stremax = max([bonds[i].get_Strength() for i in range(bond_num)])
-stremin = min([bonds[i].get_Strength() for i in range(bond_num)])
-streinter = stremax - stremin
+brain_layer = 10
+#
 
-bonds = sorted(bonds, key=attrgetter('strekey'),reverse=False)
+ip = 0
 
-bond_crod = [bonds[i].get_Cord() for i in range(bond_num)]
-bond_stre = [bonds[i].get_Strength() for i in range(bond_num)]
+### Programs ###
 
-for i in range(bond_num):
-    x, y = bond_crod[i]
-    color = (stremax-bond_stre[i])/streinter
-    colors = [color,color,color]
-    plt.plot(x,y,color=colors)
+brain2 = nr.Brain('proto',brain_layer)
+
+print('Run 1')
+brain2.get_Cell(0).obtain_Charge(1.1)
+over_charg = []
+if brain2.get_Cell(0).check_Charge():
+    over_charg = brain2.move_Charge(0)
+print('Run 2')
+over_charg2 = []
+num = len(over_charg)
+for i in range(num):
+    over_charg2.append(brain2.move_Charge(over_charg[i]))
 
 
-plt.xlim(-10,10)
-plt.ylim(-10,10)
-
-plt.xlabel("x-axis")
-plt.ylabel("y-axis")
-plt.title("The Title")
 if ip == 1:
-    plt.show()
+# Get all cells
+    cells = brain2.get_AllCell()
+    tools.plot_cells(cells)
 
+# Get all bonds
+    bonds = brain2.get_AllBond()
+    tools.plot_bonds(bonds)
 
-
-# For animation
-fig = plt.figure()
-ax = plt.axes(xlim=(-1,10), ylim=(-1, 5))
-line, = ax.plot([], [], lw=2)
-
-def init():
-    line.set_data([], [])
-    return line,
-
-def animate(i):
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
-
-#anim = animation.FuncAnimation(fig, animate, init_func=init,frames=200, interval=20, blit=True)
-
-#anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-
-#plt.show()
-
-
-
-
-
+    tools.plot_frame(brain_layer+1)
 
--- src/neural.py
+++ src/neural.py
@@ -5,11 +5,6 @@
 #
 from tools import rotate2D, sum_vector, neg_vector
 
-def get_AllCellLocation(self):
-    len = self.get_CellNum()
-    x = [self.get_Cell(i).get_Cord()[0] for i in range(len)]
-    y = [self.get_Cell(i).get_Cord()[1] for i in range(len)]
-    return x, y
 
 class Brain:
 
@@ -18,7 +13,7 @@ class Brain:
         self.__ring = 1 + num
         self.__cell_num = 1 + 3*(1+num)*num
         
-        self.__cells = [Cell(i,0.,0.) for i in range(self.__cell_num)]
+        self.__cells = [Cell(i,(0.,0.)) for i in range(self.__cell_num)]
 
 # Set the coordinate of cells
         self.__cells[0].set_Cord((0.,0.,))
@@ -63,28 +58,49 @@ class Brain:
     def get_AllBond(self):
         bonds = []
         for i in range(self.__cell_num):
-            cell_bonds = self.__cells[i].get_Bond()
+            cell_bonds = self.__cells[i].get_Bonds()
             for k in range(len(cell_bonds)):
                 bonds.append(cell_bonds[k])
         return bonds
         
+    def get_AllCell(self):
+        return self.__cells
         
     def get_Cell(self,ind):
         return self.__cells[ind]
     def get_CellNum(self):
         return self.__cell_num
 
+    def move_Charge(self,ID):
+        print('============= Charge release from',ID,' =============')
+        over_charg = []
+        cell = self.get_Cell(ID)
+        bonds = cell.get_Bonds()
+        for i in range(cell.get_AxonNum()):
+            bond = bonds[i]
+            targ_cell = self.get_Cell(bond.get_TargID())
+            targ_cell.obtain_Charge(bond.get_Strength())
+            if targ_cell.check_Charge():
+                over_charg.append(bond.get_TargID())
+            print (bond.get_Strength())
+            bond.StrengthenBond()
+            print ('Charge goes to: ',targ_cell.get_Name(),bond.get_Strength())
+        cell.release_Charge()
+        return over_charg
+
 class Cell:
 
-    def __init__(self,name,i,j):
+    def __init__(self,name,cord):
         self.__name = name
-        self.__cord = (i,j)
+        self.__cord = cord
         self.__connect_to = []
         self.__connect_from = []
         self.__cell_size = 1
         self.__dendrite_num = 0
         self.__axon_num = 0
         self.__bond = []
+        self.__charge = Charge(self.__cord,0.)
+        self.__threshold = 1.
 #        self.__indn[name] = __ind
 #        print ('Create a Cell "',self.__name,'" with index:',self.__ind)
     
@@ -92,7 +108,7 @@ class Cell:
         cord = targ.get_Cord()
         self.__axon_num += 1
         self.__connect_to.append(cord)
-        self.__bond.append(Bond(self.get_Cord(),targ.get_Cord()))
+        self.__bond.append(Bond(self,targ))
         targ.ConnectFrom(self)
 #        print ('Connect',self.__name,'to ',targ.__name)
 
@@ -128,7 +144,7 @@ class Cell:
         return self.__connect_from
     def get_ConnectTo(self):
         return self.__connect_to
-    def get_Bond(self):
+    def get_Bonds(self):
         return self.__bond
     def get_BondNum(self):
         return len(self.__bond)
@@ -137,18 +153,30 @@ class Cell:
         return [self.__bond[x].get_Strength() for x in range(i)]
     
     def get_BondCord(self):
-        x = [self.get_Bond[i].get_Start[0] for i in range(len(self.__bond))]
-        y = [self.get_Bond[i].get_Start[1] for i in range(len(self.__bond))]
+        x = [self.get_Bonds[i].get_Start[0] for i in range(len(self.__bond))]
+        y = [self.get_Bonds[i].get_Start[1] for i in range(len(self.__bond))]
         return x, y
-
-
+    
+    def get_Charge(self):
+        return self.__charge
+    
+    def obtain_Charge(self,inte):
+        self.__charge.marge(inte)
+    
+    def release_Charge(self):
+        self.__charge.set_Inte(self.__charge.get_Inte()-self.__threshold)
+        
+    def check_Charge(self):
+        if self.__charge.get_Inte() >= self.__threshold:
+            return True
 
 class Bond:
 
-    def __init__(self,cords,corde):
-        self.__start = cords
-        self.__end = corde
-        self.__distance = ((cords[0]-corde[0])**2+(cords[1]-corde[1])**2)**0.5
+    def __init__(self,cell,targ):
+        self.__targID = targ.get_Name()
+        self.__start = cell.get_Cord()
+        self.__end = targ.get_Cord()
+        self.__distance = ((self.__start[0]-self.__end[0])**2+(self.__start[1]-self.__end[1])**2)**0.5
         self.__strength = 1./self.__distance
         self.strekey = 1./self.__distance
 
@@ -167,3 +195,32 @@ class Bond:
         return self.__strength
     def get_Distance(self):
         return self.__distance
+    def get_TargID(self):
+        return self.__targID
+
+
+
+class Charge:
+
+    def __init__(self,cord,intensity):
+        self.__cord = cord
+        self.__inte = intensity
+
+    def marge(self,pulse):
+        self.__inte += pulse
+    
+
+    def move(self,bond):
+        cord = bond.get_End()
+        stre = bond.get_Strength()
+
+    def get_Inte(self):
+        return self.__inte
+
+    def set_Inte(self,inte):
+            self.__inte = inte
+
+
+
+
+
